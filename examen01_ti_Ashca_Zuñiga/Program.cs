edfc53f868709be04bfedd866ab6c0c315a3893e
Deivid Ashca <ashcadeivi@gmail.com>
2025-10-08 11:31:04 -0500

    ASHCA ZUÑIGA: 11H30

--- examen01_ti_Ashca_Zuñiga/Program.cs
+++ examen01_ti_Ashca_Zuñiga/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace examen01_ti_Ashca_Zuñiga
+﻿using System.Text;
+
+namespace examen01_ti_Ashca_Zuñiga
 {
     public class Program
     {
         static void Main(string[] args)
         {
-           
+            AZ_IGranOperacion suma = new AZ_SumarNumeros();
+            AZ_IValidarGranDato validarDatos = new AZ_ValidarDatos();
+            var calculadora = new AZ_Calculador(suma,validarDatos);
         }
     }
 }
+
