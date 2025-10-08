56b5a17070cbafa114e01acb09a3a578111929e3
Deivid Ashca <ashcadeivi@gmail.com>
2025-10-08 10:44:51 -0500

    ASHCA ZUÑIGA: 10H45 INICIO DEL PROYECTO

---
+++ examen01_ti_Ashca_Zuñiga/AZ_IGranOperacion.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace examen01_ti_Ashca_Zuñiga;
+
+// Aplicacion del principio (SRP) Single Responsibility Principle 
+internal interface AZ_IGranOperacion
+{
+    public string ExjecutarOperacion(string numero1, string numero2);
+}
---
+++ examen01_ti_Ashca_Zuñiga/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace examen01_ti_Ashca_Zuñiga
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
