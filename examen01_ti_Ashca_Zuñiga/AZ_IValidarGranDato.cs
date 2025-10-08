52b6b97af8cb112eebca74845f132a7263d677bb
Deivid Ashca <ashcadeivi@gmail.com>
2025-10-08 11:01:21 -0500

    ASHCA ZUÑIGA: 11H00

---
+++ examen01_ti_Ashca_Zuñiga/AZ_IValidarGranDato.cs
@@ -0,0 +1,8 @@
+﻿
+
+namespace examen01_ti_Ashca_Zuñiga;
+
+public interface AZ_IValidarGranDato
+{
+    public bool verificarDatos(string dato);
+}
---
+++ examen01_ti_Ashca_Zuñiga/AZ_ValidarDatos.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System.Security;
+using System.Text;
+
+namespace examen01_ti_Ashca_Zuñiga;
+
+public class AZ_ValidarDatos : AZ_IValidarGranDato
+{
+    public bool verificarDatos(string dato)
+    {
+        if (string.IsNullOrEmpty(dato) || dato.Length < 1) 
+        {
+            return false; // No nulo, no vacío, al menos 1 dígito
+        }
+        return true;
+    }
+}
